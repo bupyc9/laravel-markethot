f295009ac07d0d62f4ba508fa19a62cd091cc859
Afanasyev Pavel <bupyc9@gmail.com>
2018-10-18 11:17:11 +0300

    model Product

---
+++ app/Product.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Product
+ *
+ * @property int $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property string $name
+ * @property string $picture
+ * @property string $description
+ * @property float $price
+ * @property int $amount
+ * @property string $external_id
+ * @method static Builder|Product whereAmount($value)
+ * @method static Builder|Product whereCreatedAt($value)
+ * @method static Builder|Product whereDeletedAt($value)
+ * @method static Builder|Product whereDescription($value)
+ * @method static Builder|Product whereExternalId($value)
+ * @method static Builder|Product whereId($value)
+ * @method static Builder|Product whereName($value)
+ * @method static Builder|Product wherePicture($value)
+ * @method static Builder|Product wherePrice($value)
+ * @method static Builder|Product whereUpdatedAt($value)
+ * @mixin \Eloquent
+ * @method static bool|null forceDelete()
+ * @method static QueryBuilder|Product onlyTrashed()
+ * @method static bool|null restore()
+ * @method static QueryBuilder|Product withTrashed()
+ * @method static QueryBuilder|Product withoutTrashed()
+ */
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = ['name', 'picture', 'description', 'price', 'amount', 'external_id'];
+}
---
+++ database/migrations/2018_10_17_131718_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+            $table->string('name');
+            $table->string('picture');
+            $table->text('description');
+            $table->float('price');
+            $table->integer('amount');
+            $table->string('external_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+}
